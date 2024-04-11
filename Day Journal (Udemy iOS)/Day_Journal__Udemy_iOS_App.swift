5fd4a7ca877a4f393356f13a75b6433b895a0f66
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-11 19:36:16 -0300

    Initial Commit

---
+++ Day Journal (Udemy iOS)/Day_Journal__Udemy_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_Journal__Udemy_iOS_App.swift
+//  Day Journal (Udemy iOS)
+//
+//  Created by Pedro Marques on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_Journal__Udemy_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
